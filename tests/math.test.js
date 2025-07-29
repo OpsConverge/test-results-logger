e58f8481c5b2c0f044c0179998ef34418be87edd
OpsConverge <OpsConverge@gmail.com>
2025-07-29 00:26:34 -0600

    Initial test setup

---
+++ tests/math.test.js
@@ -0,0 +1,9 @@
+describe('Math tests', () => {
+  it('should pass', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  it('should fail', () => {
+    expect(2 + 2).toBe(5); // intentional fail
+  });
+});
