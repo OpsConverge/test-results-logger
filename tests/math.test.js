fc2ec74485d888ce0278f41878bf02c2c18f0c12
OpsConverge <OpsConverge@gmail.com>
2025-08-05 15:16:20 -0600

    flakiness_1

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,9 +1,15 @@
-describe('Math tests', () => {
-  it('should pass', () => {
-    expect(1 + 1).toBe(2);
-  });
+// describe('Math tests', () => {
+//   it('should pass', () => {
+//     expect(1 + 1).toBe(2);
+//   });
 
-  it('should fail', () => {
-    expect(2 + 2).toBe(11); // intentional fail
-  });
+//   it('should fail', () => {
+//     expect(2 + 2).toBe(11); // intentional fail
+//   });
+// });
+
+test('randomly flaky test', () => {
+  const pass = Math.random() > 0.5;
+  expect(pass).toBe(true);
 });
+
