d6a9d6ebd913bf4e484a901d02ebebaf6bcf0322
OpsConverge <OpsConverge@gmail.com>
2025-08-05 15:56:23 -0600

    fl

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,21 +1,21 @@
-// describe('Math tests', () => {
-//   it('should pass', () => {
-//     expect(1 + 1).toBe(2);
-//   });
+describe('Math tests', () => {
+  it('should pass', () => {
+    expect(1 + 1).toBe(2);
+  });
 
-//   it('should fail', () => {
-//     expect(2 + 2).toBe(11); // intentional fail
-//   });
-// });
+  it('should fail', () => {
+    expect(2 + 2).toBe(11); // intentional fail
+  });
+});
 
 // test('randomly flaky test', () => {
 //   const pass = Math.random() > 0.5;
 //   expect(pass).toBe(true);
 // });
 
-test('intentionally flaky test', () => {
-  const run = process.env.TEST_RUN || Math.floor(Math.random() * 100);
-  const isFlaky = run % 2 === 0;
-  expect(isFlaky).toBe(true);
-});
+// test('intentionally flaky test', () => {
+//   const run = process.env.TEST_RUN || Math.floor(Math.random() * 100);
+//   const isFlaky = run % 2 === 0;
+//   expect(isFlaky).toBe(true);
+// });
 
